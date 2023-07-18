6cb77c00ef55701bce77b31d92d5b6426a5fbb44
David King <David@kinglawnc.com>
2023-07-18 10:51:22 -0400

    Initial commit

---
+++ src/components/Faqs.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+import { Container } from '@/components/Container'
+
+
+
+export function Faqs() {
+  return (
+    <section>
+      
+INSERT ABOUT US SECTION FROM: https://kinglawnc.com/about/
+
+
+    </section>
+
+  )
+}
