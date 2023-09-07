527682552fc55f0c30cf00833713333b3fc71b70
A3DataSolutions <119904503+A3DataSolutions@users.noreply.github.com>
2023-09-07 00:27:15 +0000

    modified footer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   experimental: {
-    scrollRestoration: true,
-    outputStandalone: true
+    scrollRestoration: true
   },
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,8 @@ function QrCodeBorder(props) {
 
 export function Footer() {
   return (
-    <div className="bg-black pt-10 pb-20 text-white text-center">
+    //Inserted specific styling to center footer as per David
+    <div style={{paddingBottom : '2.5rem'}} className="bg-black pt-10 pb-20 text-white text-center">
       <span className='block m-1' >King @ Law</span>
       <span className='block m-1' >6613 Hammersmith Drive</span>
       <span className='block m-1' >Raleigh, NC 27613</span>
