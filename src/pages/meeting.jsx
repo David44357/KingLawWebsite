53543bf0a27a2c41b2089c556df6006d12fee72b
A3DataSolutions <119904503+A3DataSolutions@users.noreply.github.com>
2023-10-01 15:55:13 +0000

    calendar

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -64,16 +64,11 @@ export default function MyApp() {
       <Header />
 
       <div>
-        <HeroBanner title="MEETING" />
 
 
         <div className="bg-white py-12 lg:py-16">
           <Container>
-            <h4 className="text-center text-3xl font-medium uppercase tracking-tight text-gray-900">
-              Select an option <span className="text-[#ff551d]"></span>
-            </h4>
-            <label className="text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-            <select id="countries" onChange={handleDropdownChange} className="text-center p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select id="countries" onChange={handleDropdownChange} className="mx-auto max-w-md text-center p-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
               <option value="Select a value">Please choose an option</option>
               <option value="kinglawnc/consultation">I want a consultation</option>
               <option value="kinglawnc/meeting">I am a current client</option>
