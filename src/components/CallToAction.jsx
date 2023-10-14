1fae50d6e224cab726b74cf1f6f7d9ab51e9da64
David44357 <David44357@gmail.com>
2023-10-14 09:59:02 -0400

    Update CallToAction.jsx

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -26,7 +26,7 @@ export function CallToAction() {
           <div className="mt-8 flex justify-center">
             {/* <AppStoreLink color="white" /> */}
             <Button
-              href={'https://k-l-get-a-quote.flutterflow.app/?fbclid=IwAR1OvslkR4bLihv7jj-F6F7pUFt-8RxGl3UZuhzHvDEXxISSFHL6XPQvfWg'}
+              href={'https://apps.kinglawnc.com/selector'}
               color={true ? '#000' : 'gray'}
               className="mt-6 text-black text-[1.10rem] bg-gray-50 px-8 py-4 rounded-md"
               aria-label={`Get started with the Quote Tool`}
