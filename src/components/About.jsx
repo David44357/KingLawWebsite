aea70939e5a07c3ec3ae59268b0fe3d47bbe0906
David44357 <David44357@gmail.com>
2023-12-27 23:19:48 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ export function About() {
           <div className="md:shrink-0">
             <Image src={MyTeamImage} alt="Image" className="h-96 w-full object-cover md:h-full md:w-96" />
           </div>
-          <div className="py-8 px-6 md:p-8">
+          <div className="py-8 px-6 mb-10 md:p-8">
             <div className="uppercase block mt-1 mb-10 text-3xl leading-tight font-medium text-black">About <span className='text-[#ff551d]'>Our</span> Firm</div>
             <p className="text-gray-600 mt-1 mb-5 text-[1.11rem] sm:text-[1rem] lg:text-[1.11rem] ">
               King @ Law is run by a husband/wife team: Attorney David King and Paralegal Maryann Voisinet (we kept our last names). We work out of a home-office in Raleigh, North Carolina.
