24ce34de5021f0211c064cc5cbcd6b949839209a
David44357 <David44357@gmail.com>
2023-12-27 23:57:37 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,18 +12,18 @@ export function About() {
 
   return (
     <section className={clsx(
-      'flex flex-col overflow-hidden rounded-3xl shadow-lg shadow-gray-900/5 text-gray-300 py-12 bt-10 lg:py-16',
+      'flex flex-col overflow-hidden rounded-3xl shadow-lg shadow-gray-900/5 text-gray-300 py-12 lg:py-16',
       featured ? 'order-first bg-gray-900 lg:order-none' : 'bg-gray-100'
     )}
       id='about'
     >
-      <div className="max-w-lg mx-auto mb-10 bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl cursor-pointer">
+      <div className="max-w-lg mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl cursor-pointer">
         <div className="flex flex-col md:flex-row">
           <div className="md:shrink-0">
             <Image src={MyTeamImage} alt="Image" className="h-96 w-full object-cover md:h-full md:w-96" />
           </div>
-          <div className="py-8 px-6 mb-10 md:p-8">
-            <div className="uppercase block mt-1 mb-10 text-3xl leading-tight font-medium text-black">About <span className='text-[#ff551d]'>Our</span> Firm</div>
+          <div className="py-8 px-6 md:p-8">
+            <div className="uppercase block mt-1 mb-6 text-3xl leading-tight font-medium text-black">About <span className='text-[#ff551d]'>Our</span> Firm</div>
             <p className="text-gray-600 mt-1 mb-5 text-[1.11rem] sm:text-[1rem] lg:text-[1.11rem] ">
               King @ Law is run by a husband/wife team: Attorney David King and Paralegal Maryann Voisinet (we kept our last names). We work out of a home-office in Raleigh, North Carolina.
             </p>
@@ -32,6 +32,6 @@ export function About() {
       </div>
 
     </section>
-<BR>
+
   )
 }
