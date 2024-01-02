f08cfbba2c044b591f53c56cf18dea7b57bd3faf
David44357 <David44357@gmail.com>
2024-01-02 02:04:06 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -333,10 +333,10 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              A modern approach to law Test
+              A modern approach to law
             </h1>
             <p className="mt-6 text-lg text-gray-600">
-              {'Hiring a modern law firm means options for any budget, convenient online meetings, and an app that streamlines the process.'}
+              {'Hiring a modern law firm means options for any budget, convenient online meetings, and a portal that streamlines the process. Can\'t afford a lawyer? Use our online DIY apps with attorney support for just $199 a month.'}
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               {/* <AppStoreLink /> */}
