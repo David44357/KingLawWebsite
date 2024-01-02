5467b24998e871cd2c014eeb39d91cba494a5f3b
David44357 <David44357@gmail.com>
2024-01-02 01:23:45 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -336,7 +336,7 @@ export function Hero() {
               A modern approach to law Test
             </h1>
             <p className="mt-6 text-lg text-gray-600">
-              {'Hiring a modern law firm means convenient online meetings, flat-fee payment plans, and an app that streamlines the entire process. Can\'t afford us? Use our online do-it-yourself apps with attorney support for just $199 a month. '}
+              {'Hiring a modern law firm means options for any budget, convenient online meetings, and an app that streamlines the process.'}
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               {/* <AppStoreLink /> */}
