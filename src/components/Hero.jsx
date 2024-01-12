add413e91ea775d31ec4caefc2ae97945a616f7a
David44357 <David44357@gmail.com>
2024-01-12 01:44:21 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -343,7 +343,7 @@ export function Hero() {
               <Cal></Cal>
             </div>
           </div>
-          <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
+          <div className="relative mt-5 sm:mt-10 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
             <BackgroundIllustration className="absolute left-3/4 top-4 h-[613px] w-[513px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_top,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
             <div style={{bottom: "-8rem"}} className="[bottom: -8rem] hidden lg:block max-w-300 [mask-image:linear-gradient(to_bottom,white_70%,transparent)] sm:mx-36 md:mx-40 lg:absolute lg:mx-5 lg:bottom-32 lg:-top-10 lg:h-auto lg:px-0">
               <div className="[mask-image:linear-gradient(to_bottom,white_70%,transparent)]">
