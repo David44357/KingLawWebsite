8a687f72943e8e150742d19c9bc8abc436d049ca
David44357 <David44357@gmail.com>
2024-01-16 13:31:36 -0500

    Update PromHero.jsx

--- src/components/PromHero.jsx
+++ src/components/PromHero.jsx
@@ -328,7 +328,7 @@ function AppDemo() {
 
 export function PromHero() {
   return (
-    <div className="overflow-hidden lg:pb-30 xl:pb-28">
+    <div className="overflow-hidden lg:pb-36 xl:pb-32">
       <Container>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
           <div className="">
