f540cc1bdc5eebd2147ba25b4ae8b9ffcbb73064
David44357 <David44357@gmail.com>
2024-01-16 11:43:48 -0500

    Update PromPrimaryFeatures.jsx

--- src/components/PromPrimaryFeatures.jsx
+++ src/components/PromPrimaryFeatures.jsx
@@ -71,7 +71,7 @@ export function PromPrimaryFeatures() {
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
             4 Steps to Complete a Legal Process
           </h2>
-          <p className="mt-6 text-lg tracking-tight text-orange-100">
+          <p className="mt-6 text-lg tracking-tight text-slate-50">
           TurboTax does your taxes. Our apps do your divorce.
           </p>
         </div>
