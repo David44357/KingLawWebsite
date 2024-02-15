acf886763a71db496971eefb550319fe99701d89
David King <David@kinglawnc.com>
2024-02-15 18:47:04 -0500

    Test pull.rebase true

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -365,7 +365,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              The modern law firm Test
+              The modern law firm
             </h1>
             <p className="mt-0 text-lg text-gray-600 mt-3">
               {'What does it mean to hire a modern law firm? Getting more done for less. Trading downtown traffic for convenient Zoom meetings. Payment plans and flat-fees that include expenses. An online app to track deadlines, sign our contract, upload evidence, and more. Can\'t afford a lawyer? Use our self-service apps, starting at just $89/mo.'}
