653347d6c78b0e9edcb73e7997155dba418862fc
David44357 <David44357@gmail.com>
2024-03-11 03:59:07 -0400

    Update PromCallToAction.jsx

--- src/components/PromCallToAction.jsx
+++ src/components/PromCallToAction.jsx
@@ -17,9 +17,6 @@ export function PromCallToAction() {
     if (platform.includes('android')) {
       setUserOS('https://play.google.com/store/apps/details?id=com.KingLaw.kinglawfinal'); // CHANGE HERE FOR ANDROID
       setOS('android')
-    } else if (platform.includes('iphone') || platform.includes('ipad') || platform.includes('ipod')) {
-      setUserOS('https://apps.apple.com/app/prometheus-nc-divorce-law/id6475014498'); // CHANGE HERE FOR IOS
-      setOS('apple')
     } else {
       setUserOS('https://apps.kinglawnc.com'); // CHANGE HERE FOR OTHER
       setOS('other')
@@ -27,11 +24,7 @@ export function PromCallToAction() {
   }, []);
 
   function ButtonLink(props) {
-    if (props.OS != 'other') {
-      return <AppStoreLink />;
-    } else {
       return   <Button href={props.userOS} variant="outline" className="m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Web Portal</Button>
-    }
   }
 
   return (
