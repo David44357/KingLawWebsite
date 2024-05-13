e25e8315720401fe4e280ef0741151348f2f54d9
Muhammad Usama mashkkoor <usamamashkoor@gmail.com>
2024-05-13 06:57:59 +0500

    icon apps

--- src/components/PromSecondaryFeatures.jsx
+++ src/components/PromSecondaryFeatures.jsx
@@ -104,24 +104,35 @@ function Feature({ feature, isActive, className, ...props }) {
       className={clsx(className, !isActive && 'opacity-75 hover:opacity-100')}
       {...props}
     >
-      <div
-        className={clsx(
-          'w-9 rounded-lg',
-          isActive ? 'bg-blue-600' : 'bg-slate-500',
-        )}
-      >
-        <svg aria-hidden="true" className="h-9 w-9" fill="none">
-          <feature.icon />
-        </svg>
+      <div class="flex gap-2">
+        <div
+          className={clsx(
+            'w-9 rounded-lg ',
+            isActive ? 'bg-blue-600' : 'bg-slate-500',
+          )}
+        >
+          <svg aria-hidden="true" className="h-9 w-9" fill="none">
+            <feature.icon />
+          </svg>
+        </div>
+        <h3
+          className={clsx(
+            'mt-[2px] text-lg font-medium',
+            isActive ? 'text-blue-600' : 'text-slate-600',
+          )}
+        >
+          {feature.name}
+        </h3>
+
       </div>
-      <h3
+      {/* <h3
         className={clsx(
           'mt-3 text-sm font-medium',
           isActive ? 'text-blue-600' : 'text-slate-600',
         )}
       >
         {feature.name}
-      </h3>
+      </h3> */}
       <p className="mt-2 font-display text-xl text-slate-900">
         {feature.summary}
       </p>
