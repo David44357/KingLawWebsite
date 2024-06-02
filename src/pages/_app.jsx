6713f1a5da4f838e633356ebba95f2b77eb4361b
Muhammad Usama mashkkoor <usamamashkoor@gmail.com>
2024-06-02 05:13:27 +0500

    adding google analytics:

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,14 @@ import "animate.css"
 import "aos/dist/aos.css";
 import '@/styles/comingsoonstyles.css'
 import '@/styles/prometheusstyles.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-KFJDNTMF3C" />
+    </>
+  )
 }
