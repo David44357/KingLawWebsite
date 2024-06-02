da641ae9b5048362b5365c1a4e738c3ef85dec37
Muhammad Usama mashkkoor <usamamashkoor@gmail.com>
2024-06-02 06:09:53 +0500

    test ga

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,14 +4,28 @@ import "animate.css"
 import "aos/dist/aos.css";
 import '@/styles/comingsoonstyles.css'
 import '@/styles/prometheusstyles.css'
-import { GoogleAnalytics } from '@next/third-parties/google'
+// import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   // return <Component {...pageProps} />
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KFJDNTMF3C"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-KFJDNTMF3C');
+        `}
+      </Script>
       <Component {...pageProps} />
-      <GoogleAnalytics gaId="G-KFJDNTMF3C" />
+      {/* <GoogleAnalytics gaId="G-KFJDNTMF3C" /> */}
     </>
   )
 }
