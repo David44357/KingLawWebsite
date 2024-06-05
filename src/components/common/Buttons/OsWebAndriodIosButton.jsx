ea906294d7fdcfca570230f462a7510a0950a1bd
Muhammad Usama mashkkoor <usamamashkoor@gmail.com>
2024-06-06 04:59:41 +0500

    click event

--- src/components/common/Buttons/OsWebAndriodIosButton.jsx
+++ src/components/common/Buttons/OsWebAndriodIosButton.jsx
@@ -10,6 +10,7 @@ import { Cal } from '@/components/Cal'
 import { Container } from '@/components/Container'
 import myImage from '@/images/Suit.png'
 import backgroundImage from '@/images/background-call-to-action.jpg'
+import { sendGAEvent } from '@next/third-parties/google'
 
 
 const OsWebAndriodIosButton = (props) => {
@@ -27,7 +28,9 @@ const OsWebAndriodIosButton = (props) => {
       setOS('other')
     }
   }, []);
-
+  const btnOnClickGetStarted = () => {
+    sendGAEvent({ event: 'get_started_home_page_btn_click', value: 'home_get_started_home_page_btn_click' })
+  }
   const ButtonLink = (props) => {
     if (props.OS != 'other') {
       return <AppStoreLink andriodBtnClassess={props.andriodBtnClassess} />;
@@ -36,7 +39,12 @@ const OsWebAndriodIosButton = (props) => {
         <>
           {/* <Button href={props.userOS} variant="outline" className="m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Get Started</Button> */}
           {/* <Button href={props.userOS} variant="outline" className={`m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${webBtnClassess}`}>Get Started</Button> */}
-          <Button href={props.userOS} variant="outline" className={`${webBtnClassess}`}>Get Started</Button>
+          <Button 
+            href={props.userOS} variant="outline" className={`${webBtnClassess}`}
+            onClick={() => {
+              btnOnClickGetStarted();
+            }}
+          >Get Started</Button>
         </>
       )
     }
