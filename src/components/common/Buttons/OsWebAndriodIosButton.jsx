eb85ee5ab8fbc3bc3e079540e2c4b78676248a4a
Muhammad Usama mashkkoor <usamamashkoor@gmail.com>
2024-06-06 05:39:19 +0500

    click event

--- src/components/common/Buttons/OsWebAndriodIosButton.jsx
+++ src/components/common/Buttons/OsWebAndriodIosButton.jsx
@@ -11,12 +11,14 @@ import { Container } from '@/components/Container'
 import myImage from '@/images/Suit.png'
 import backgroundImage from '@/images/background-call-to-action.jpg'
 import { sendGAEvent } from '@next/third-parties/google'
-
+import { useRouter } from 'next/router'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const OsWebAndriodIosButton = (props) => {
   const {webBtnClassess, andriodBtnClassess} = props;
   const [userOS, setUserOS] = useState('');
   const [OS, setOS] = useState('');
+  const router = useRouter()
   useEffect(() => {
     const platform = window.navigator.userAgent.toLowerCase();
     //WHEN YOUR READY CHANGE 
@@ -30,7 +32,11 @@ const OsWebAndriodIosButton = (props) => {
   }, []);
   const btnOnClickGetStarted = () => {
     sendGAEvent({ event: 'get_started_home_page_btn_click', value: 'get_started_home_page_btn_click' })
-    alert('btnOnClickGetStarted')
+    console.log("props.userOS")
+    console.log("props.userOS")
+    console.log(props.userOS)
+    // router.push(props.userOS);
+    // alert('btnOnClickGetStarted')
   }
   const ButtonLink = (props) => {
     if (props.OS != 'other') {
@@ -41,7 +47,9 @@ const OsWebAndriodIosButton = (props) => {
           {/* <Button href={props.userOS} variant="outline" className="m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Get Started</Button> */}
           {/* <Button href={props.userOS} variant="outline" className={`m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${webBtnClassess}`}>Get Started</Button> */}
           <Button 
-            href={props.userOS} variant="outline" className={`${webBtnClassess}`}
+            href={props.userOS} 
+            // type="button"
+            variant="outline" className={`${webBtnClassess}`}
             onClick={() => {
               btnOnClickGetStarted();
             }}
@@ -55,6 +63,7 @@ const OsWebAndriodIosButton = (props) => {
   return (
     <>
       <ButtonLink OS={OS} userOS={userOS} andriodBtnClassess={andriodBtnClassess} ></ButtonLink>
+      <GoogleAnalytics gaId="G-KFJDNTMF3C" />
     </>
   )
 }
