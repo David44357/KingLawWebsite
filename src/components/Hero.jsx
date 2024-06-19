40607bbc39d50b5c8d82a3a050a724137f38b41f
David44357 <David44357@gmail.com>
2024-06-19 13:21:53 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -364,7 +364,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              legal Services for Everybody
+              Legal Services for All
             </h1>
             <p className="mt-0 text-lg text-gray-600 mt-3">
               {'Other law firms focus on serving the wealthy with hourly-rate contracts for ulimited budgets. At King @ Law, we think legal services should be accessible to everyone, not just the wealthy.'}
