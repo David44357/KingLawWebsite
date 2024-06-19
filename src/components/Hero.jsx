4d546ee41720e995b5ed4081aa2d07dc938749aa
David44357 <David44357@gmail.com>
2024-06-19 13:25:46 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -367,7 +367,7 @@ export function Hero() {
               Legal Services for Everybody
             </h1>
             <p className="mt-0 text-lg text-gray-600 mt-3">
-              {'Other law firms focus on serving the wealthy with hourly-rate contracts for ulimited budgets. At King @ Law, we think legal services should be accessible to everyone, not just the wealthy.'}
+              {'Other law firms focus on serving the wealthy with hourly-rate contracts for unlimited budgets. At King @ Law, we think legal services should be accessible to everyone, not just the wealthy.'}
               </p><p className="text-lg text-gray-600 mt-3">
               {'That\'s why we\'ve created a streamlined alternative focused on your needs, rather than billable hours. We offer flat-fees, payment plans, and an affordable do-it-yourself option through our self-service apps.'}
             </p>
