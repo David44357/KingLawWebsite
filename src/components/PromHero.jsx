532d278e29536aa445ee6feac1f349c0948c6627
David44357 <David44357@gmail.com>
2024-09-27 09:27:15 -0400

    Update PromHero.jsx

--- src/components/PromHero.jsx
+++ src/components/PromHero.jsx
@@ -354,8 +354,7 @@ export function PromHero() {
   return (
     <div
       className="bg-cover bg-center  w-full"
-      style={{ backgroundImage: `url('/images/webp/background-call-to-action-optiwebp.webp')` }}
-      // style={{ backgroundImage: `url('/images/background-call-to-action.jpg')` }}
+      style={{  background: 'linear-gradient(to right, #ff7e5f, #feb47b)'  }}
       
     >
       <div className="overflow-hidden "
