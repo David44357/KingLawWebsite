7574609ea892e3c4695e289e8f8056da861d44af
David44357 <David44357@gmail.com>
2024-09-30 21:32:12 -0400

    Update OsWebAndriodIosButton.jsx

--- src/components/common/Buttons/OsWebAndriodIosButton.jsx
+++ src/components/common/Buttons/OsWebAndriodIosButton.jsx
@@ -44,8 +44,8 @@ const OsWebAndriodIosButton = (props) => {
     } else {
       return (
         <>
-          {/* <Button href={props.userOS} variant="outline" className="m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Get Started</Button> */}
-          {/* <Button href={props.userOS} variant="outline" className={`m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${webBtnClassess}`}>Get Started</Button> */}
+          {/* <Button href={props.userOS} variant="outline" className="m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Create Account</Button> */}
+          {/* <Button href={props.userOS} variant="outline" className={`m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${webBtnClassess}`}>Create Account</Button> */}
           <Button 
             href={props.userOS} 
             // type="button"
