706835c40a32a682fad3c23c821d09cc3e03b5ae
David44357 <David44357@gmail.com>
2024-09-30 22:11:49 -0400

    Update PopOverOsWebAndriodIosButton.jsx

--- src/components/common/Buttons/PopOver/PopOverOsWebAndriodIosButton.jsx
+++ src/components/common/Buttons/PopOver/PopOverOsWebAndriodIosButton.jsx
@@ -37,7 +37,7 @@ const PopOverOsWebAndriodIosButton = (props) => {
         <>
           {/* <Button href={props.userOS} variant="outline" className="m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Compass</Button> */}
           {/* <Button href={props.userOS} variant="outline" className={`m-3 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${webBtnClassess}`}>Compass</Button> */}
-          <Button href={props.userOS} variant="outline" className={`${webBtnClassess}`}>Get Started</Button>
+          <Button href={props.userOS} variant="outline" className={`${webBtnClassess}`}>Compass</Button>
         </>
       )
     }
