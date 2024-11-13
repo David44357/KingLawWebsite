3541309f1be2b5b5588190a8c958080ac621d05e
David44357 <David44357@gmail.com>
2024-11-13 14:55:24 -0500

    Add files via upload

---
+++ src/components/createaccountbutton.jsx
@@ -0,0 +1,22 @@
+/* First make sure that you have installed the package */
+/* If you are using yarn */
+// yarn add @calcom/embed-react
+/* If you are using npm */
+// npm install @calcom/embed-react
+import { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+import { Button } from './Button'
+
+export function Cal({ selectedOption }) {
+    useEffect(() => {
+        (async function () {
+            const cal = await getCalApi();
+            cal("ui", { "styles": { "branding": { "brandColor": "#FF551C" } }, "hideEventTypeDetails": false, "layout": "month_view" });
+        })();
+    }, [])
+    return (<button
+    data-cal-link="kinglawnc/consultation"
+    data-cal-config='{"layout":"month_view"}'
+    type="button"
+    className="justify-center rounded-lg bg-amber-400 py-3 font-semibold outline-2 outline-offset-2 transition-colors text-black hover:bg-gray-900 active:bg-gray-800 text-base px-6 rounded">Initial Consultation {selectedOption} </button>);
+};
