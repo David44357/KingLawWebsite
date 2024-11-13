009904f6fc44dc1aef8652d5a4b079db2ebef570
David44357 <David44357@gmail.com>
2024-11-13 14:27:01 -0500

    Update Cal.jsx

--- src/components/Cal.jsx
+++ src/components/Cal.jsx
@@ -18,5 +18,5 @@ export function Cal({ selectedOption }) {
     data-cal-link="kinglawnc/consultation"
     data-cal-config='{"layout":"month_view"}'
     type="button"
-    className="justify-center rounded-lg bg-amber-400 px-3 h-9 text-sm font-semibold outline-2 outline-offset-2 transition-colors text-white hover:bg-gray-900 active:bg-gray-800 active:text-white/80  text-white font-medium px-4 rounded">Consultation {selectedOption} </button>);
+    className="justify-center rounded-lg bg-amber-400 py-9 text-sm font-semibold outline-2 outline-offset-2 transition-colors text-black hover:bg-gray-900 active:bg-gray-800 font-large px-6 rounded">Consultation {selectedOption} </button>);
 };
