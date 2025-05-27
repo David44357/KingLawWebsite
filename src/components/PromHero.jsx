77454890bb48ea361db53295352a1a5561c253f4
David44357 <David44357@gmail.com>
2025-05-27 02:49:30 -0400

    Update PromHero.jsx

--- src/components/PromHero.jsx
+++ src/components/PromHero.jsx
@@ -382,7 +382,7 @@ export function PromHero() {
                 data-aos="fade-up"
                 data-aos-duration="1600"
               > */}
-              <div style={{ width: 'fit-content' }} className="mx-auto bg-black shadow-lg bg-opacity-20 rounded-md mt-6 mb-6 p-5"><p className="font-bold text-3xl text-white max-w-[740px] mx-auto"
+              <div style={{ width: 'fit-content' }} className="mx-auto bg-black shadow-lg bg-opacity-30 rounded-md mt-6 mb-6 p-5"><p className="text-3xl text-white max-w-[740px] mx-auto"
                 data-aos="fade-up"
                 data-aos-duration="1600"
               >
